7240cdf3e1de73d37e374cb57c08b8b5eb0bc93d
Jean luc Tuyishime <jeanluctuyishime@Jeans-MacBook-Pro.local>
2020-03-31 19:08:54 +0200

    add new changes

--- api/server/src/helpers/mailer/templates/resetPassword.js
+++ api/server/src/helpers/mailer/templates/resetPassword.js
@@ -3,17 +3,17 @@ import { generate as generateToken } from '../../tokens';
 
 dotenv.config();
 
+
 export default (data) => {
-    console.log('data::', data);
-    const message = {};
-    const token = generateToken({ email: data.email }, { expiresIn: '1h' });
-    const appUrl = process.env.APP_URL_FRONTEND;
-    const link = `${appUrl}/reset-password/${token}`;
-    message.subject = 'Reset your password - Hospital Vaccines';
-    message.html = `Hello ${data.names} </br>,
+  const message = {};
+  const token = generateToken({ email: data.email }, { expiresIn: '1h' });
+  const appUrl = process.env.APP_URL_FRONTEND;
+  const link = `${appUrl}/reset-password/${token}`;
+  message.subject = 'Reset your password - Hospital Vaccines';
+  message.html = `Hello ${data.names} </br>,
   <p>You are receiving this because you have requested the reset of the password,<br>
    Click on the reset link bellow to complete the process<br>
   <br><br><br>
 <a href='${link}' style="margin:35px 0;padding:15px 35px;background:#266cef;color:#ffffff;clear:both;border-radius:30px;text-decoration:none" target='_blank'>Resert password Now</a></p>`;
-    return message;
+  return message;
 };
